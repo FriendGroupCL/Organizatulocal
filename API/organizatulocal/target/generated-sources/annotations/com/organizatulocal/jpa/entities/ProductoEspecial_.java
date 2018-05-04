c26117cddd402bb89fb2c4f3cda30e6c81be4ebd
jordy098 <jordyp60@gmail.com>
2018-05-03 23:58:33 -0400

    Creacion de Rest API

--- API/organizatulocal/target/generated-sources/annotations/com/organizatulocal/jpa/entities/ProductoEspecial_.java
+++ API/organizatulocal/target/generated-sources/annotations/com/organizatulocal/jpa/entities/ProductoEspecial_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T16:09:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T23:30:16")
 @StaticMetamodel(ProductoEspecial.class)
 public class ProductoEspecial_ { 
 
