b3d54b47e0ac263ab02ee279c44a056836c5a784
jordy098 <jordyp60@gmail.com>
2018-05-03 23:58:33 -0400

    Creacion de Reset de API

--- API/organizatulocal/target/generated-sources/annotations/com/organizatulocal/jpa/entities/Permiso_.java
+++ API/organizatulocal/target/generated-sources/annotations/com/organizatulocal/jpa/entities/Permiso_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T16:09:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T23:30:16")
 @StaticMetamodel(Permiso.class)
 public class Permiso_ { 
 
